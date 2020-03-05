1225b4c7127ee72d7b5a343a6d50838a0f959116
ZhangYi <loeyae@gmail.com>
2020-03-05 20:12:09 +0800

    优化错误提示页

--- app/errors/GeneralError.php
+++ app/errors/GeneralError.php
@@ -3,24 +3,21 @@
  * GeneralError.php
  *
  * @author  Zhang Yi <loeyae@gmail.com>
- * @version SVN: $Id: Zhang Yi $
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License
+ * @version GIT: $id $
  */
 ?>
 <!DOCTYPE html>
-<!--
-Licensed under the Apache License, Version 2.0 (the "License"),
-see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
--->
 <html>
     <head>
         <title>出错了</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link type="text/css" rel="stylesheet" href="/static/css/bootstrap.css" />
+        <link type="text/css" rel="stylesheet" href="/css/bootstrap.css" />
     </head>
     <body>
         <header id="header" class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
-            <a class="navbar-brand mr-0 mr-md-2" href="/"><img class="logo" src="/static/images/favicon.png"></a>
+            <a class="navbar-brand mr-0 mr-md-2" href="/"></a>
             <div class=""></div>
         </header>
         <div class="container-fluid min-vh-100">
@@ -28,26 +25,36 @@ see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
                 <a href="/" class="float-left col-auto">首页</a><?php if (!empty($_SERVER['HTTP_REFERER'])) { ?><a href="<?= $_SERVER['HTTP_REFERER']; ?>" class="float-right col-auto">返回前页</a><?php } ?>
             </nav>
             <section id="content" class="text-center">
+                    <?php
+                    $appConfig = $context->getAppConfig();
+                    $debug = $appConfig ? $appConfig->getSetting('debug', false) : false;
+                    if ($debug) {
+                        ?>
+                <p class="text-danger text-left ml-lg-5">
+                <?php
+                        print_r(nl2br($e->getMessage()));
+                        echo '<br /><br />';
+                        print_r(nl2br($e->getTraceAsString()));
+                    } else {
+                        ?>
                 <p class="text-danger">
                 <?php
-                if ($e instanceof \loeye\error\ResourceException) {
-                    $message = '找不到了';
-                } else {
-                    $message = '内部错误';
-                }
-                echo $message;
-                ?>
+                        if ($e instanceof \loeye\error\ResourceException) {
+                            $message = '找不到了';
+                        } else {
+                            $message = '出错了，请稍候再试～';
+                        }
+                        echo $message;
+                    }
+                    ?>
                 </p>
             </section>
         </div>
         <footer id="footer" class="text-black-50 col-12 text-center mt-1 mb-4">
             <div id="copyright"><span>©<?= date('Y'); ?>&nbsp;</span><span>Loeyae.com&nbsp;</span><span>ICP证：蜀ICP备14020223号-1</span></div>
         </footer>
-        <script type="text/javascript" src="/static/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="/js/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="/bootstrap/dist/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
-<!--[CDATA[
-        <?php print_r(debug_backtrace()) ?>
-]]>
